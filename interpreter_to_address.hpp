8d78772eb00a46a7fe943843a0defe2d6619b5ac
Zakirov Rishat <zakirovrishat.me@gmail.com>
2021-05-20 19:12:38 +0300

    copy code from main to class

--- interpreter_to_address.hpp
+++ interpreter_to_address.hpp
@@ -13,9 +13,23 @@ public:
 	template <typename T>
 	void print(const T value);
 
+	template<>
+	void print(const std::string value);
+
+	template<class... T>
+	void print(const std::tuple<T...>& _tup);
+
+	/*
+	template<typename T>
+	void print(T value_container)
+	*/
+
 private:
+	template<class TupType, size_t... I>
+	void print(const TupType& _tup, std::index_sequence<I...>);
+
 	template <typename T, size_t size>
-	void printArrayAsIp(const std::array<T, size> bytes_array);
+	void printContainerAsIp(const std::array<T, size> bytes_array);
 
 	template <typename T>
 	std::array<uint8_t, sizeof(T)> reinterpretToBytes(const T value);
@@ -26,7 +40,7 @@ template <typename T>
 void InterpreterToAddress::print(const T value)
 {
 	const auto bytes_array = reinterpretToBytes(value);
-	printArrayAsIp(bytes_array);
+	printContainerAsIp(bytes_array);
 }
 
 template <typename T>
@@ -43,14 +57,36 @@ std::array<uint8_t, sizeof(T)> InterpreterToAddress::reinterpretToBytes(const T 
 	return bytes_array;
 }
 
-
 template <typename T, size_t size>
-void InterpreterToAddress::printArrayAsIp(const std::array<T, size> bytes_array)
+void InterpreterToAddress::printContainerAsIp(const std::array<T, size> bytes_array)
 {
+	size_t index = 0;
 	for(const auto byte_value : bytes_array)
 	{
-		std::cout << static_cast<size_t>(byte_value) << ".";
+		std::cout << static_cast<size_t>(byte_value);
+		if(++index < bytes_array.size())
+			std::cout << ".";
 	}
+	std::cout << std::endl;
+}
+
+template<>
+void InterpreterToAddress::print(const std::string value)
+{
+	std::cout << value << std::endl;
+};
+
+template<class... T>
+void InterpreterToAddress::print(const std::tuple<T...>& _tup)
+{
+	print(_tup, std::make_index_sequence<sizeof...(T)>());
+}
+
+
+template<class TupType, size_t... I>
+void InterpreterToAddress::print(const TupType& _tup, std::index_sequence<I...>)
+{
+	(..., (std::cout << (I == 0 ? "" : ".") << std::get<I>(_tup)));
 	std::cout << std::endl;
 }
 
