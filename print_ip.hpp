fa8e6e15546c2516eeab160dd13193559b72bda4
Zakirov Rishat <zakirovrishat.me@gmail.com>
2021-05-31 16:05:26 +0300

    make custom type_traits file and moved all the checks there

--- print_ip.hpp
+++ print_ip.hpp
@@ -11,10 +11,10 @@
 
 #include <iostream>
 #include <algorithm>
-#include <type_traits>
 #include <string>
 #include <array>
 #include <list>
+#include "custom_type_traits.h"
 
 template <typename T>
 std::array<uint8_t, sizeof(T)> reinterpretToBytes(const T value)
@@ -102,10 +102,6 @@ auto print_ip(T value) -> decltype(
  *
  *******************************************************************************
  */
-template < class T>
-struct is_string : std::integral_constant <bool,
-									 std::is_same<std::string, T>::value ||
-									 std::is_same<const char*, T>::value >{};
 template <typename T>
 auto print_ip(const std::string value) -> decltype(
 		std::declval<
@@ -114,9 +110,6 @@ auto print_ip(const std::string value) -> decltype(
 {
 	std::cout << value << std::endl;
 }
-
-template <typename> struct is_tuple : std::false_type {};
-template <typename... T> struct is_tuple<std::tuple<T...>> : std::true_type {};
 
 template<class TupType, size_t... index>
 void print_tuple(const TupType& tuple_value, std::index_sequence<index...>)
@@ -164,32 +157,6 @@ auto print_ip(T value) -> decltype(
 	print_tuple(value);
 }
 
-template<typename T, typename _ = void>
-struct is_container : std::false_type {};
-
-template<typename... Ts>
-struct is_container_helper {};
-
-template<typename T>
-struct is_container<
-		T,
-		std::conditional_t<
-			false,
-			is_container_helper<
-				typename T::value_type,
-				typename T::size_type,
-				typename T::allocator_type,
-				typename T::iterator,
-				typename T::const_iterator,
-				decltype(std::declval<T>().size()),
-				decltype(std::declval<T>().begin()),
-				decltype(std::declval<T>().end()),
-				decltype(std::declval<T>().cbegin()),
-				decltype(std::declval<T>().cend())
-				>,
-			void
-			>
-		> : public std::true_type {};
 
 template <typename T>
 auto print_ip(T value) -> decltype(
